3859aed88eadf5d558a100c2a9a08d5687ae073e
John Thielman <john.thielman@gmail.com>
2020-03-02 12:22:03 -0600

    Add project files.

---
+++ Reckoner/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reckoner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me some numbers to multiply, separated by commas.  Please.");
+            var input = Console.ReadLine();
+            var numberStrings = input.Split(",");
+            var product = 1;
+            foreach(var num in numberStrings)
+            {
+                product = int.Parse(num) * product;
+            }
+            Console.WriteLine(product);
+            Console.ReadLine();
+        }
+    }
+}
